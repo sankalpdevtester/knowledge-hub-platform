961c593f324e0e7e7c8f7fe6fc2ed887fa126dc0
sankalpdevtester <ohofufic0@gmail.com>
2026-06-18 17:54:49 +0530

    feat: add Discussion model for discussion forums feature

---
+++ lib/models/discussion_model.dart
@@ -0,0 +1,68 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+class DiscussionModel extends Equatable {
+  final String id;
+  final String title;
+  final String description;
+  final String categoryId;
+  final String userId;
+  final List<String> comments;
+  final Timestamp createdAt;
+  final Timestamp updatedAt;
+
+  DiscussionModel({
+    @required this.id,
+    @required this.title,
+    @required this.description,
+    @required this.categoryId,
+    @required this.userId,
+    @required this.comments,
+    @required this.createdAt,
+    @required this.updatedAt,
+  });
+
+  factory DiscussionModel.fromJson(Map<String, dynamic> json) {
+    return DiscussionModel(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+      categoryId: json['categoryId'],
+      userId: json['userId'],
+      comments: List<String>.from(json['comments']),
+      createdAt: json['createdAt'],
+      updatedAt: json['updatedAt'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      'categoryId': categoryId,
+      'userId': userId,
+      'comments': comments,
+      'createdAt': createdAt,
+      'updatedAt': updatedAt,
+    };
+  }
+
+  @override
+  List<Object> get props => [
+        id,
+        title,
+        description,
+        categoryId,
+        userId,
+        comments,
+        createdAt,
+        updatedAt,
+      ];
+
+  @override
+  String toString() {
+    return 'DiscussionModel(id: $id, title: $title, description: $description, categoryId: $categoryId, userId: $userId, comments: $comments, createdAt: $createdAt, updatedAt: $updatedAt)';
+  }
+}
